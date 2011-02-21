a798559603260887c4f4f5b28d252ffe588d575a
Thomas L. Johnsen <thomas@alltid.org>
2011-02-21 01:32:30 +0100

    Moved .vim to vim

---
+++ vim/createtags.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ctags -f tags -h ".php" -R --exclude="\.svn" --totals=yes --tag-relative=yes --PHP-kinds=+cf --regex-PHP='/abstract class ([^ ]*)/\1/c/' --regex-PHP='/interface ([^ ]*)/\1/c/' --regex-PHP='/(public |static |abstract |protected |private )+function ([^ (]*)/\2/f/' --exclude="\.git"
